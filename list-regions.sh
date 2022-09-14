fdb3961683aae2333db17a0f2e853b59b9d30ece
nguyenmin <mnguyenn@protonmail.com>
2022-09-14 13:32:59 -0700

    Print regions as list

--- list-regions.sh
+++ list-regions.sh
@@ -1,7 +1,8 @@
 reg=()
 while read object; do
-    reg+=("${object}")
     regionNames=($(az account list-locations --query "[? displayName == '${object}'].name" --output tsv))
-    echo "region ${regionNames[0]}"
+    #echo "region ${regionNames[0]}"
+    reg+=("${regionNames[0]}")
 done < <(az provider show -n Microsoft.ContainerService | jq -r '.resourceTypes[] | select(.resourceType=="managedClusters") | .locations[]')
 echo "Number of aks regions ${#reg[@]}"
+echo "List of regions ${reg[@]}"
